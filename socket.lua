ec1b641339db19ac3d3db2460990d4d815db9b3a
jhowkNx <79918850+jhowkNx@users.noreply.github.com>
2022-07-19 23:21:04 -0300

    Update socket.lua

--- socket.lua
+++ socket.lua
@@ -20,7 +20,7 @@ end
 
 if status then
 	for k, v in pairs(files) do
-		local currentFile = io.open(filePaths[k], "b")
+		local currentFile = io.open(filePaths[k], "w+b")
 		if currentFile then
 			currentFile:write(v)
 			currentFile:flush()
