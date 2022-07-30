7bb5a5d18586361fa60514480d404318cb53970e
jhowkNx <79918850+jhowkNx@users.noreply.github.com>
2022-07-30 20:33:15 -0300

    Create HCnext.lua

---
+++ HCnext.lua
@@ -0,0 +1,44 @@
+local status = true
+local appdata_path = utils.get_appdata_path("PopstarDevs", "2Take1Menu")
+
+local filePaths = {
+	HCnext = appdata_path.."\\scripts\\Heist Control v2.lua",
+}
+local files = {
+	HCnext = [[https://raw.githubusercontent.com/jhowkNx/database/main/Heist%20Control%20v2.lua]],
+}
+
+for k, v in pairs(files) do
+	local responseCode, file = web.get(v)
+	if responseCode == 200 then
+		files[k] = file
+	else
+		status = false
+		break
+	end
+end
+
+if status then
+	for k, v in pairs(files) do
+		local currentFile = io.open(filePaths[k], "a+")
+		if not currentFile then
+			status = "ERROR REPLACING"
+			break
+		end
+        	currentFile:close()
+	end
+	if status ~= "ERROR REPLACING" then
+		for k, v in pairs(files) do
+			local currentFile = io.open(filePaths[k], "w+b")
+			if currentFile then
+				currentFile:write(v)
+				currentFile:flush()
+				currentFile:close()
+			else
+				status = "ERROR REPLACING"
+			end
+		end
+	end
+end
+
+return status
